07d654650dc128e290b1b07541ff295eb7ed7cbf
lynnhourieh <lynnhourieh30@gmail.com>
2023-09-29 18:27:05 +0300

    ui navbar

--- airbnb/app/components/nabvar/UserMenu.tsx
+++ airbnb/app/components/nabvar/UserMenu.tsx
@@ -1,7 +1,13 @@
 "use client";
-import {AiOutlineMenu} from "react-icons/ai"
+import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useState, useCallback } from "react";
+import MenuItem from "./MenuItem";
 export const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
   return (
     <div className="relative ">
       <div className="flex flex-row items-center gap-3">
@@ -12,7 +18,7 @@ export const UserMenu = () => {
           Airbnb your home
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="rounded-full p-4 md:py-1
         md:px-2 border-[1px]
         border-neutral-200
@@ -24,13 +30,22 @@ export const UserMenu = () => {
         hover:shadow-md
         transition"
         >
-            <AiOutlineMenu/>
-            <div className="hidden md:block">
-                <Avatar/>
-
-            </div>
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
         </div>
       </div>
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <> <MenuItem onClick={() => {}} label="Sign up" />
+              <MenuItem onClick={() => {}} label="login" />
+             
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
